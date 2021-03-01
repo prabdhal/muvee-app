079f7aad9299e871afdac16f0d28f7d213078111
Prabdeep Dhaliwal <prab.dhaliwal95@gmail.com>
2021-03-01 08:56:32 -0500

    hover effect show movie details

--- app.js
+++ app.js
@@ -191,64 +191,6 @@ document.onclick = function (e) {
     content.classList.remove('content-display');
   }
 };
-
-// <td> under the mouse right now (if any)
-let currentElem = null;
-
-document.onmouseover = function (e) {
-  // before entering a new element, the mouse always leaves the previous one
-  // if currentElem is set, we didn't leave the previous <td>,
-  // that's a mouseover inside it, ignore the event
-  if (currentElem) return;
-
-  let target = e.target.closest('movies');
-
-  // we moved not into a <td> - ignore
-  if (!target) return;
-
-  // moved into <td>, but outside of our table (possible in case of nested tables)
-  // ignore
-  if (!document.contains(target)) return;
-
-  // hooray! we entered a new <td>
-  currentElem = target;
-  onEnter(currentElem);
-};
-
-document.onmouseout = function (e) {
-  // if we're outside of any <td> now, then ignore the event
-  // that's probably a move inside the table, but out of <td>,
-  // e.g. from <tr> to another <tr>
-  if (!currentElem) return;
-
-  // we're leaving the element – where to? Maybe to a descendant?
-  let relatedTarget = e.relatedTarget;
-
-  while (relatedTarget) {
-    // go up the parent chain and check – if we're still inside currentElem
-    // then that's an internal transition – ignore it
-    if (relatedTarget == currentElem) return;
-
-    relatedTarget = relatedTarget.parentNode;
-  }
-
-  // we left the <td>. really.
-  onLeave(currentElem);
-  currentElem = null;
-};
-
-// any functions to handle entering/leaving an element
-function onEnter(elem) {
-  console.log('entered', elem);
-
-  elem.style.opacity = 0.1;
-}
-
-function onLeave(elem) {
-  console.log(elem);
-  elem.style.opacity = 1;
-}
-
 // Toggle Search Bar on/off
 searchButton.onclick = (e) => {
   console.log(searchMovieFormElement);
